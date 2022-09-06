00e9b61635f0863112c7991d6ea23351b5d4e841
Evgeniy Petrov <silkshadow90@gmail.com>
2022-09-06 16:49:28 +0400

    added some fixes

--- components/Intro/ExhibitionCard.tsx
+++ components/Intro/ExhibitionCard.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import React, {PropsWithChildren} from 'react';
 import styles from '../../styles/Ex.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
     title: string;
@@ -12,13 +14,15 @@ interface Props {
 
 const ExhibitionCard = ({ title, text, css, csstwo, link }: PropsWithChildren<Props>) => {
     return (
-        <a href={link}>
-            <div className={css}>
-                <div className={csstwo}/>
-                <div className={styles.exhibition_Main__cardTitle}>{title}</div>
-                <div className={styles.exhibition_Main__cardText}>{text}</div>
+        <Link href={link}>
+            <div className={css} style={{ position: 'relative', minHeight: 'auto', overflow: 'hidden', transition: 'all 200ms linear', border: 'none' }}>
+                <div className={csstwo} style={{ margin: 0 }}/>
+                <div style={{ position: 'absolute', background: '#ffffffbb', bottom: 0, left: 0, right: 0, height: '6rem' }}>
+                    <div className={styles.exhibition_Main__cardTitle}>{title}</div>
+                    <div className={styles.exhibition_Main__cardText}>{text}</div>
+                </div>
             </div>
-        </a>
+        </Link>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,17 @@ import {Page} from "../components/Page";
 import {Join} from "../components/Joined";
 import { Intro } from '../components/Intro';
 import SimpleSlider from "../components/Slider";
+import Image from 'next/image';
+
+function jpeg(size: number = 150) {
+  return `https://via.placeholder.com/${size}`
+}
+
+function getImages(length: number, size: number = 150) {
+  return new Array(length).fill(null).map((_, index) => (
+    <Image loader={() => jpeg(size)} src={jpeg(size)} key={index} width={`${size}px`} height={`${size}px`} objectFit={'contain'}/>
+  ))
+}
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +24,10 @@ const Home: NextPage = () => {
     {/*<div className={styles.titleList}>*/}
     {/*</div>*/}
           <SimpleSlider/>
+
+        <div>
+          {getImages(10, 150)}
+        </div>
           <div className={styles.infoUpper}>{Strings.main.title}</div>
           <div className={styles.info}>
               <div className={styles.infoOver}>
