3e937aa184d8b5db36681b7c682ff3d677fd23b6
Evgenii Petrov <silkshadow90@gmail.com>
2022-10-25 18:11:17 +0400

    Create nextjs.yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  // assetPrefix: isProd ? '/simba/' : '',
+  images: {
+    loader: 'akamai',
+    path: ''
+  }
+}
