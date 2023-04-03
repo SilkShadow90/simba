4017d554f182cae576bd4c99a4238b18b7597dec
Evgeniy Petrov <silkshadow90@gmail.com>
2023-04-03 20:52:46 +0300

    fix images

--- components/ExhibitionCard.tsx
+++ components/ExhibitionCard.tsx
@@ -1,13 +1,12 @@
 import React, { PropsWithChildren } from 'react';
 import Link from 'next/link';
-import Image, { StaticImageData } from 'next/image';
 import styles from '../styles/Ex.module.css';
 
 interface Props {
     title: string;
     text: string;
     link: string;
-    image?: StaticImageData
+    image?: string
     opacityBlock?:boolean
     hoverBlock?:boolean
 }
@@ -16,12 +15,7 @@ const ExhibitionCard = ({ title, text, link, image, opacityBlock, hoverBlock }: 
     return (
         <Link href={link}>
             <div className={hoverBlock ? styles.ex : styles.ex_noHover}>
-                {image && (
-                  <div className={styles.ex_card}>
-                      {/* eslint-disable-next-line jsx-a11y/alt-text */}
-                    <Image src={image} objectFit={'cover'} layout={'fill'} />
-                  </div>
-                )}
+                <div className={styles.ex_card} style={image ? { background: `url(${image})`,backgroundSize:"cover", objectFit: 'cover' }: undefined}/>
                 <div className={opacityBlock ? styles.ex_opacity : styles.ex_opacity_DisplayNone}>
                     <div className={styles.ex_title}>{title}</div>
                     <div className={styles.ex_description}>{text}</div>
--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ module.exports = {
   images: {
     loader: 'akamai',
     path: '/',
-    domains: ['source.unsplash.com'],
   },
   output: 'standalone',
   eslint: {
